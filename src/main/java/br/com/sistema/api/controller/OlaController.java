5213c7a2c4b9b2bb59b795b7cb7cbd690e6fda7b
Victor <vmerling@gmail.com>
2026-08-21 11:38:06 -0300

    2nd commit

--- src/main/java/br/com/sistema/api/controller/OlaController.java
+++ src/main/java/br/com/sistema/api/controller/OlaController.java
@@ -1,5 +1,20 @@
 package br.com.sistema.api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController //Registra a classe como um controller do Spring, permitindo que ela manipule requisições HTTP
+@RequestMapping("ola") //Define o caminho (ou url) base para todos os metodos da classe
+
 public class OlaController {
     
+    @GetMapping("/olamundo")
+    
+    public String olaMundo () {
+        return "Olá Mundo!!!";
+    }
 }
+
+//
--- src/main/java/br/com/sistema/api/controller/PacienteController.java
+++ src/main/java/br/com/sistema/api/controller/PacienteController.java
@@ -1,5 +1,23 @@
 package br.com.sistema.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/paciente")
 public class PacienteController {
+    // GET
+    @GetMapping("/")
+    public String exibirHome(){
+        return "a";
+    }
+
+    // POST
+    // PUT
+    // DELETE
+    
+    // CRUD
     
 }
+
